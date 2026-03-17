3dd085d7dc3a7fcbacd14b59d6cd408e7fe31c53
Quentin <quentin.chiniard@gmail.com>
2026-03-17 12:19:14 +0100

    implementation de la fonction inspector

---
+++ src/tool.py
@@ -0,0 +1,29 @@
+def inspector (df):
+    print("######### DEBUT DE L'ANALYSE #########")
+
+    # fonction permetant de donner le nombres de ligne et de colonnes
+    nb_ligne = df.shape[0]
+    print(f"Le df comprend {df.shape[0]} lignes et {df.shape[1]} colonnes ")
+    print('\n---------------------------------------------------------------')
+
+    #fonction comptant le nombre de nulls
+    count_null = df.isna().sum()
+    print(f"le df contient les nuls suivant ")
+    print(count_null)
+    print('\n---------------------------------------------------------------')
+
+    #fonction donnant le nom et le type de données 
+    print(f"les types et noms de colonnes sont les suivants")
+    print(f"{df.dtypes}")
+    print('\n---------------------------------------------------------------')
+
+    #fonction donnant le nombre d'unique par colonnes
+    nb_unique = df.nunique()
+    print("le nombre d'uniques dans les colonnes sont les suivants")
+    print(f"{df.nunique()}")
+    print('\n---------------------------------------------------------------')
+
+    #if nb_unique == nb_ligne :
+    #    print("il y a une potentiel PK ")
+
+    print("\n######### FIN DE L'ANALYSE #########")
